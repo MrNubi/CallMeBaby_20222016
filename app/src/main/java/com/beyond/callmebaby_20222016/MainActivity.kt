2ff933bc51ffd056d7f6aff30ce090491a654f5e
MrNubi <99630193+MrNubi@users.noreply.github.com>
2022-02-16 11:54:41 +0900

    전화 걸기 코드 작성 CALL Action -> 권한 없어서 강제종료 + Send to 기능 + sms_body 통해서 내용 전달 가능, 얘도 인텐트니까 + 웹으로 바로 보내기 / 마켓으로 바로 보내기

--- app/src/main/java/com/beyond/callmebaby_20222016/MainActivity.kt
+++ app/src/main/java/com/beyond/callmebaby_20222016/MainActivity.kt
@@ -53,7 +53,9 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("sms_body", EDT_SMS)
             //이름 고정, 우리가 컨트롤 불가, 쟤내가 만든대로 붙여줘야됨
 
+
             startActivity(intent)
+            //문자전송 활용처: 지정된 번호로 문자에 맞게 보내면 그걸로 본인 인증,
 
         }
 
@@ -67,6 +69,10 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_VIEW, mUrii)
 
             startActivity(intent)
+            //광고 링크 + 앱 다운로드 광고 링크도 이쪽으로 많이 해결
+            // 사이트를 하나 만들고, 거기다가 마켓 링크 넣기
+            // 안드로이드 / 아이폰 유저 편하게 나누기
+            // 약관 보여줄 때, PDF하나 만들어놓고 전문 보기 -> 그 파일 보여주기
 
         }
 
@@ -81,6 +87,8 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_VIEW, mUrii)
 
             startActivity(intent)
+
+            //업데이트 안하면 못 하게 강제한 후, 업데이트하라고 마켓으로 보낼 때
 
         }
 
