d82c340088cbd55466f0a42bcfbe8791c9bec222
MrNubi <99630193+MrNubi@users.noreply.github.com>
2022-02-16 11:23:43 +0900

    전화 걸기 코드 작성 CALL Action -> 권한 없어서 강제종료 + Send to 기능 + sms_body 통해서 내용 전달 가능, 얘도 인텐트니까

--- app/src/main/java/com/beyond/callmebaby_20222016/MainActivity.kt
+++ app/src/main/java/com/beyond/callmebaby_20222016/MainActivity.kt
@@ -42,12 +42,16 @@ class MainActivity : AppCompatActivity() {
 
         btn_Message.setOnClickListener {
 
-            val phoneNumber = EDT_Phone.text.toString()
+            val phoneNumber = EDT_Phone.text.toString()//EDT_SMS
+            val EDT_SMS = EDT_SMS.text.toString()
 
             //인텐트는 항상 어디론가 향해있어야 함, URI로 알려줘라
             val mUrii = Uri.parse("smsto:$phoneNumber")//띄어쓰기는 오류 원인
 
             val intent = Intent(Intent.ACTION_SENDTO, mUrii)
+
+            intent.putExtra("sms_body", EDT_SMS)
+            //이름 고정, 우리가 컨트롤 불가, 쟤내가 만든대로 붙여줘야됨
 
             startActivity(intent)
 
